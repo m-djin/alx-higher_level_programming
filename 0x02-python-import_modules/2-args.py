6bfaf4f831ffd57106d7aefe809dbe95edd5f47a
m-djin <mnkdjin@st.ug.edu.gh>
2024-01-04 22:07:20 -0800

    d

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     y = len(argv) - 1
     if y < 1:
-        print("{} arguments".format(y))
+        print("{} arguments.".format(y))
     elif y == 1:
         print("{} argument:".format(y))
     else:
