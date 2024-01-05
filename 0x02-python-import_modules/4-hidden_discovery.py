066481fe47817a9944ed238e4cdf52f01e84791c
m-djin <mnkdjin@st.ug.edu.gh>
2024-01-04 21:28:05 -0800

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-        if allf[i][:2] != "__":
-            print("{:s}".format(allf[i]))
+    allfun = dir()
+    for i in range(0, len(allfun)):
+        if allfun[i][:2] != "__":
+            print("{:s}".format(allfun[i]))
