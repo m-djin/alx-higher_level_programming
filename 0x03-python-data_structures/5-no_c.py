71a1412385b844270e9edd7eeb9f7e12fd003b87
m-djin <mnkdjin@st.ug.edu.gh>
2024-01-08 08:33:22 -0800

    task 10

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,3 @@ def no_c(my_string):
         new_string = my_string.translate({ord("c"): None})
         next_string = new_string.translate({ord("C"):None})
         return next_string
-    return my_string
