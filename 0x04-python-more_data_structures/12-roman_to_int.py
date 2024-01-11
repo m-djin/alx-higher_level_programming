572f8c1bc14832f90f9b3e3dafcf74d49d8e088d
m-djin <mnkdjin@st.ug.edu.gh>
2024-01-10 17:55:00 -0800

    task 11

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,39 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not roman_string:
         return 0
-    roman_dec = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_no = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_dec[roman_string[j]] > roman_dec[roman_string[j - 1]]:
-            roman_no += roman_dec[roman_string[j]] - 2 * \
-                        roman_dec[roman_string[j - 1]]
-        else:
-            roman_no += roman_dec[roman_string[j]]
-    return roman_no
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_key = list(rom_num.keys())
+
+    digit = 0
+    last_r = 0
+    list_digit = [0]
+
+    for i in roman_string:
+        for j in list_key:
+            if j == i:
+                if rom_num.get(i) <= last_r:
+                    digit += to_subtract(list_digit)
+                    list_digit = [rom_num.get(i)]
+                else:
+                    list_digit.append(rom_num.get(i))
+
+                last_r = rom_num.get(i)
+
+    digit += to_subtract(list_digit)
+
+    return (digit)
