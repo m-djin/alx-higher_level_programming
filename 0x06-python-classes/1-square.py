ee11f469f2bcac7e29fa27881569dde1551779cf
m-djin <mnkdjin@st.ug.edu.gh>
2024-01-23 16:35:41 -0800

    classes and onjects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
